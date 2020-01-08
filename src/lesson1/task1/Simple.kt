7dc0e31e01815dffd025c1b77008c9fa3c3119da
panku-tat <panku.tanita@gmail.com>
2020-01-08 23:47:09 +0700

    Lession 1: Решены 2 задачи уровня "Тривиальная". Перевод длины в метры и перевод угла в градусы.

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -68,7 +68,7 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes*2.1336) + (arshins*0.7112) + (vershoks*0.0445)
 
 /**
  * Тривиальная
@@ -76,7 +76,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double = (deg + (min / 60) + (sec / 360)) * PI / 180
 
 /**
  * Тривиальная
